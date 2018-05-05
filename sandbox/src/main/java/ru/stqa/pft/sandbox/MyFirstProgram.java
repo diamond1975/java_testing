9075466e30da9350c7187837d8af86606d24e252
Almaz <diamond1975@yandex.ru>
2018-05-06 01:20:30 +0300

    Математические операции, функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,23 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+
+    System.out.println(2+2);
+    System.out.println(2*2);
+    System.out.println(2/2);
+    System.out.println(2-2);
+    System.out.println(1/2);
+    System.out.println(1.0/2);
+    System.out.println(1/3.0);
+    System.out.println(2.0/3);
+    System.out.println("2"+"4");//Делить, умножать, вычитать нельзя. Только можно сложить.
+    System.out.println("2"+4);
+    System.out.println(2+"4");
+    System.out.println(2+2*5);
+    System.out.println((2+2)*5);
+    System.out.println("2+2="+2+4);
+    System.out.println("2+2="+(2+4));
+
   }
 
 }
