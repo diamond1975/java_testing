a92fc71d06e4af34b9b71b82abbb4b656e8e7a96
Almaz <diamond1975@yandex.ru>
2018-05-11 16:37:42 +0300

    Первый тест для веб-приложения, используя драйвер Selenium

--- sandbox/src/test/java/ru/stq/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stq/pft/sandbox/PointTest.java
@@ -13,7 +13,7 @@ public class PointTest {
     Point p3 = new Point(50, 60);
 
     Assert.assertEquals(p1.distance (p2),42.638011210655684);
-    Assert.assertEquals(p2.distance (p3),28.284271247461902);
+    Assert.assertEquals(p2.distance (p3),28.284271247461901);
     Assert.assertEquals(p1.distance (p3),70.83784299369935);
   }
 }
