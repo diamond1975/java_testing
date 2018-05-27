3479067b812d64d3c4d95db8c47ae1c8298a569e
Almaz <diamond1975@yandex.ru>
2018-05-27 19:12:09 +0300

    Лекция 4.7. Множества неупорядоченные коллекции. Тесты путем сравнения списков, для удаления группы, модификации группы.

--- addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
@@ -1,9 +1,11 @@
 package ru.stqa.pft.adressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -18,11 +20,16 @@ public class GroupModificationTest extends TestBase {
     //int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("diamind", "тест33", "тест33", "тест44"));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(),"diamind", "тест33", "тест33", "тест44");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List <GroupData> after = app.getGroupHelper().getGroupList ();
     //int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
