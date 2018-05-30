fd0ba3c8aa72f30068b18c876c023b07d04e4d18
Almaz <diamond1975@yandex.ru>
2018-05-31 00:19:32 +0300

    Лекция 4.9. Форма контакта (Создание контакта). Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/tests/СontactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/tests/СontactCreationTests.java
@@ -14,7 +14,7 @@ public class СontactCreationTests extends TestBase {
     app.getNavigationHelper().gotoContactPage();
     List <ContactData> before = app.getContactHelper().getContactList ();
     //int before = app.getContactHelper().getContactCount ();
-    ContactData contact = new ContactData("Almaz", "Gabdullin", null, "Moscow, prospect Mira, " +
+    ContactData contact = new ContactData("Almaz1975", "Gabdullin", null, "Moscow, prospect Mira, " +
             "d 16, rv 25", "89651249288", "89651249236", "89671245625",
             "diamond1976@yandex.ru", "diamond1977@yandex.ru", "diamond167@yandex.ru", "diamind");
     app.getContactHelper().creatContact(contact,true);
@@ -22,14 +22,15 @@ public class СontactCreationTests extends TestBase {
     //int after = app.getContactHelper().getContactCount ();
     Assert.assertEquals(after.size(),before.size()+1);
 
-
-    int max =0;
-    for (ContactData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    contact.setId (max);
+    //int max =0;
+    //for (ContactData g : after) {
+    //  if (g.getId() > max) {
+    //   max = g.getId();
+    //  }
+    //}
+    //Comparator<? super ContactData> byId = (Comparator<ContactData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    //int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+    contact.setId (after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
