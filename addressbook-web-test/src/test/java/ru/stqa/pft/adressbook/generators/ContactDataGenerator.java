8158d28b167c0f60ebd0f12c16df18123bf82db6
Almaz <diamond1975@yandex.ru>
2018-06-13 22:40:14 +0300

    Лекция 6.8. (Группа, Контакты) Автоматическое закрытие файлов после использования

--- addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/generators/ContactDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/generators/ContactDataGenerator.java
@@ -45,44 +45,44 @@ public class ContactDataGenerator {
       saveASXml(contacts, new File(file));
     } else if (format.equals("xml")) {
       saveASXml(contacts, new File(file));
-    }else if(format.equals("json")) {
+    } else if (format.equals("json")) {
       saveASJson(contacts, new File(file));
     } else {
       System.out.println("Unrecognized format" + format);
     }
   }
+
   private void saveASJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
-    String json = gson.toJson (contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    String json = gson.toJson(contacts);
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
   private void saveASXml(List<ContactData> cotacts, File file) throws IOException {
     XStream xStream = new XStream();
     xStream.processAnnotations(ContactData.class);
-    String xml =xStream.toXML(cotacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
-  }
-  private void save(List<ContactData> contacts, File file) throws IOException {
-    System.out.println(new File(".").getAbsolutePath());
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;\n", contact.getName1(), contact.getName2(),
-              contact.getAddress(), contact.getEmail1(),contact.getEmail2(),contact.getEmail3(), contact.getMobileHome()
-                      ,contact.getMobile(),contact.getMobileWork(), contact.getGroup()));
+    String xml = xStream.toXML(cotacts);
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
     }
-    writer.close();
   }
 
-
+  private void saveASCsv (List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;\n", contact.getName1(), contact.getName2(),
+                contact.getAddress(), contact.getEmail1(), contact.getEmail2(), contact.getEmail3(), contact.getMobileHome()
+                , contact.getMobile(), contact.getMobileWork(), contact.getGroup()));
+      }
+    }
+  }
   private List<ContactData> generateContact(int count) {
     List<ContactData> contact = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
-      contact.add(new ContactData().withName1(String.format("Gabdullin %s", i)).withName2 (String.format("Almaz %s", i))
+      contact.add(new ContactData().withName1(String.format("Gabdullin %s", i)).withName2(String.format("Almaz %s", i))
               .withAddress(String.format("Moscow %s", i)).withEmail1(String.format("test@yandex.ru %s", i))
               .withEmail2(String.format("test2@yandex.ru %s", i)).withEmail3(String.format("test3@yandex.ru %s", i))
               .withMobileHome(String.format("112548932", i)).withMobile(String.format("252685", i)).withMobileWork(String.format("158322215", i)).withGroup(String.format("diamind", i)));
