72f1ba6c726999bbd298fc3d0f881c8f4e7d9d37
Almaz <diamond1975@yandex.ru>
2018-06-18 00:01:57 +0300

    Лекция 7.2. Object Relational Mapping (ORM)

--- addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
@@ -1,20 +1,36 @@
 package ru.stqa.pft.adressbook.model;
 
 import com.google.gson.annotations.Expose;
+import com.sun.javafx.beans.IDProperty;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Table;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @XStreamAlias("group")
+@Entity
+@javax.persistence.Table (name= "group_list")
 public class GroupData {
   @XStreamOmitField
+  @Id
+  @Column (name = "group_id")
   private int id = Integer.MAX_VALUE;;
   @Expose
+  @Column (name = "group_name")
   private String name;
   @Expose
+  @Column (name = "group_header")
+  @Type(type = "text")
   private String header;
   @Expose
   private String header1;
   @Expose
+  @Column (name = "group_footer")
+  @Type(type = "text")
   private String footer;
 
   public String getName() {
