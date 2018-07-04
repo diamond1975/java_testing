fbb26c594e72a88e8f8528fc82e488c241215386
Almaz <diamond1975@yandex.ru>
2018-07-05 00:27:08 +0300

    Лекция 8.7. Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере James

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,8 +1,6 @@
 package ru.stqa.pft.mantis.tests;
 
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -16,7 +14,7 @@ import static org.testng.Assert.assertTrue;
 
   public class RegistrationTests extends TestBase {
 
-    @BeforeMethod
+    //@BeforeMethod
     public void startMailServer() {
       app.mail().start();
     }
@@ -26,9 +24,11 @@ import static org.testng.Assert.assertTrue;
       long now = System.currentTimeMillis();
       String user = String.format("user%s", now);
       String password = "password";
-      String email = String.format("user%s@localhost.localdomain", now);
+      String email = String.format("user%s@localhost", now);
+      app.james().createUser(user, password);
       app.registration().start(user, email);
-      List<MailMessage> mailMessages = app.mail().waitForMail(2, 20000);
+      //List<MailMessage> mailMessages = app.mail().waitForMail(2, 20000);
+      List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
       String confirmationLink = findConfirmationLink(mailMessages, email);
       app.registration().finish(confirmationLink, password);
       assertTrue(app.newSession().login(user, password));
@@ -40,7 +40,7 @@ import static org.testng.Assert.assertTrue;
       return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+    //@AfterMethod(alwaysRun = true)
     public void stopMailServer() {
       app.mail().stop();
     }
